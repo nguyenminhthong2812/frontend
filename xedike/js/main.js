c55dbeb3a90734e01b620fe39a7beccc1d0101f9
nguyenminhthong2812 <45055992+nguyenminhthong2812@users.noreply.github.com>
2018-12-17 05:46:37 +0700

    17

--- xedike/js/main.js
+++ xedike/js/main.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
-    $('[data-toggle="popover"]').popover();   
+    $("[data-toggle=popover]").popover({
+        html : true,
+        content: function() {
+          var content = $(this).attr("data-popover-content");
+          return $(content).children(".popover-body").html();
+        },
+        title: function() {
+          var title = $(this).attr("data-popover-content");
+          return $(title).children(".popover-heading").html();
+        }
+    }); 
+
+    $('[data-toggle="tooltip"]').tooltip(); 
+
+    // chạy jquery select
+     $('select').niceSelect();
+
+     // datepicker
+     $('#datepicker').datepicker({
+            uiLibrary: 'bootstrap4'
+        });
+     //spinedit
+     $('#spinEdit').spinedit({
+		    minimum: -10000,
+		    maximum: 10000,
+		    step: 100,
+		    value: 0,
+		    numberOfDecimals: 0
+		});
 });
