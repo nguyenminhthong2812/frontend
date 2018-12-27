47757c206fe9cc464335114aa32a09de42858310
nguyenminhthong2812 <45055992+nguyenminhthong2812@users.noreply.github.com>
2018-12-27 15:17:49 +0700

    8207

---
+++ JQUERY/QLND/app/models/KhoaHoc.js
@@ -0,0 +1,8 @@
+function KhoaHoc(maKH,tenKH,moTa,hinhAnh,luotXem,nguoiTao){
+	this.maKH = maKH,
+	this.tenKH = tenKH,
+	this.moTa = moTa,
+	this.hinhAnh = hinhAnh,
+	this.luotXem = luotXem,
+	this.nguoiTao = nguoiTao
+}
