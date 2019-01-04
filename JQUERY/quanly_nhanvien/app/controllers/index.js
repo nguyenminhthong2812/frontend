862d1a93d1c1c2120053d452d257ac164c7c4141
nguyenminhthong2812 <45055992+nguyenminhthong2812@users.noreply.github.com>
2019-01-04 11:03:02 +0700

    5

--- JQUERY/quanly_nhanvien/app/controllers/index.js
+++ JQUERY/quanly_nhanvien/app/controllers/index.js
@@ -142,8 +142,8 @@ $(document).ready(function(){
     var kq1 = KiemTraRong('#maNV','#tbMaNV');
     if(kq1)
       kq1 = KiemTraDoDai('#maNV','#tbMaNV',4,6);
-  	if(kq1)
-  		kq1 = KiemTraMaNV('#maNV','#tbMaNV');
+  	// if(kq1)
+  	// 	kq1 = KiemTraMaNV('#maNV','#tbMaNV');
     var kq2 = KiemTraRong('#tenNV','#tbTenNV');
     // if(kq2)
     //   kq2 = KiemTraNhapKyTu('#tenNV','#tbTenNV');
@@ -199,7 +199,8 @@ $(document).ready(function(){
  	//XỬ LÝ NÚT THÊM
  	$('body').delegate('#them','click',function(){ 		
  		var val = Validate();
- 		if(val){
+ 		var ktraMaNV = KiemTraMaNV('#maNV','#tbMaNV');
+ 		if(val && ktraMaNV){
  			// lấy các giá trị từ input
  			var maNV = $('#maNV').val();
  			var tenNV = $('#tenNV').val();
@@ -219,7 +220,11 @@ $(document).ready(function(){
  			LuuLocalStorage(dsnv);
  			HienThiLenBang(dsnv);
  			ResetForm();
-
+ 			new PNotify({
+			    title: 'Hoàn thành!',
+			    text: 'Đã thêm nhân viên thành công.',
+			    type: 'success'
+			});
  		}
 
  	});
@@ -231,11 +236,21 @@ $(document).ready(function(){
  			// sửa lại format ngày sinh
  			var ngaySinh = dsnv.DSNV[i].NgaySinh; 									
  			var ngaySinhFormat = ngaySinh.substring(8,10) + '/' + ngaySinh.substring(5,7) + '/' + ngaySinh.substring(0,4);
+ 			// sửa lại mã nhân viên để hiển thị cho danh sách tìm kiếm
+ 			// vì khi hiển thị danh sách tìm kiếm ta có thêm thẻ span vào manhanvien
+ 			// nên phải chỉnh lại để hiển thị vào value checkbox ko bị lỗi
+ 			var maNV = dsnv.DSNV[i].MaNhanVien; 			
+ 			maNV = maNV.replace(`<span class='tukhoa'>`,'');
+ 			maNV = maNV.replace('</span>',''); 
+ 			var tenNV = dsnv.DSNV[i].TenNhanVien;
+ 			tenNV = tenNV.replace(`<span class='tukhoa'>`,'');
+ 			tenNV = tenNV.replace('</span>',''); 			
+ 			// gán dữ liệu
  			dulieu += `
 				<tr class='trNhanVien'>
-					<td><input type='checkbox' class='chkXoa' value='${dsnv.DSNV[i].MaNhanVien}'></td>
-					<td class='td-maNV' value='${dsnv.DSNV[i].MaNhanVien}'>${dsnv.DSNV[i].MaNhanVien}</td>
-					<td class='td-tenNV' value='${dsnv.DSNV[i].TenNhanVien}'>${dsnv.DSNV[i].TenNhanVien}</td>
+					<td><input type='checkbox' class='chkXoa' value='${maNV}'></td>
+					<td class='td-maNV' value='${maNV}'>${dsnv.DSNV[i].MaNhanVien}</td>
+					<td class='td-tenNV' value='${tenNV}'>${dsnv.DSNV[i].TenNhanVien}</td>
 					<td class='td-email' value='${dsnv.DSNV[i].Email}'>${dsnv.DSNV[i].Email}</td>
 					<td class='td-soDT' value='${dsnv.DSNV[i].SoDT}'>${dsnv.DSNV[i].SoDT}</td>
 					<td class='td-ngaySinh' value='${dsnv.DSNV[i].NgaySinh}'>${ngaySinhFormat}</td>
@@ -256,16 +271,36 @@ $(document).ready(function(){
 
  	// XỬ LÝ NÚT XÓA
  	$('#xoaNV').click(function(){
- 		$('.chkXoa').each(function(){
- 			if($(this).is(":checked")){
- 				var maNV = $(this).val();
- 				var viTri = TimViTri(maNV);
- 				//console.log(viTri);
- 				dsnv.XoaNhanVien(viTri); 				
- 			}
- 		});
- 		LuuLocalStorage(dsnv);
- 		HienThiLenBang(dsnv);
+
+ 		swal({
+			  title: "Bạn muốn xóa nhân viên?",
+			  text: "",
+			  icon: "warning",
+			  buttons: true,
+			  dangerMode: true,
+			})
+			.then((willDelete) => {
+			  if (willDelete) {
+			  	$('.chkXoa').each(function(){
+	 			if($(this).is(":checked")){
+	 				var maNV = $(this).val();
+	 				var viTri = TimViTri(maNV);
+	 				//console.log(viTri);
+	 				dsnv.XoaNhanVien(viTri); 				
+	 			}
+		 		});
+		 		LuuLocalStorage(dsnv);
+		 		HienThiLenBang(dsnv);
+			    
+			  } 
+
+			});
+
+
+
+ 		
+		  
+		
  	});
  	// HÀM TÌM VỊ TRÍ CỦA ĐỐI TƯỢNG TRONG MẢNG
  	function TimViTri(maNV){
@@ -288,13 +323,13 @@ $(document).ready(function(){
  		// thay đổi footer
  		var footer = `
 			<button class='btn btn-success' id="btnCapNhat">Cập nhật</button>
-			<button class='btn btn-danger' id="btnDong">Đóng</button>
+			<button class='btn btn-danger' id="dong">Đóng</button>
  		`;
  		$('.modal-footer').html(footer);
  		// lấy giá trị từ các cột		
  		var tr = $(this).closest('tr');
- 		var maNV = $(tr).find('.td-maNV').html();
- 		var tenNV = $(tr).find('.td-tenNV').html();
+ 		var maNV = $(tr).find('.td-maNV').attr('value');
+ 		var tenNV = $(tr).find('.td-tenNV').attr('value');
  		var email = $(tr).find('.td-email').html();
  		var sdt = $(tr).find('.td-soDT').html();
  		var ngaySinh = $(tr).find('.td-ngaySinh').attr('value');
@@ -312,13 +347,122 @@ $(document).ready(function(){
  		$('#soNgayLamViec').val(SNLV);
  		$('#chucVu').val(chucVu);
  		$('#phuCap').val(phuCap);
+ 		// gán thuộc tính readonly cho maNV
+ 		$('#maNV').attr('readonly','true');
  		// hiển thị modal
  		$('#btnModal').trigger('click');
  	});
+ 	// XỬ LÝ NÚT CẬP NHẬT
+ 	$('body').delegate('#btnCapNhat','click',function(){
+ 		var val = Validate();
+ 		if(val){
+ 			// lấy các giá trị từ input
+ 			var maNV = $('#maNV').val();
+ 			var tenNV = $('#tenNV').val();
+ 			var email = $('#email').val();
+ 			var soDT = $('#soDT').val();
+ 			var ngaySinh = $('#ngaySinh').val();
+ 			var luongCB = $('#luongCB').val();
+ 			var soNgayLamViec = $('#soNgayLamViec').val();
+ 			var chucVu = $('#chucVu').val();
+ 			var phuCap = $('#phuCap').val();
+ 			// tìm vị trí của nhân viên trong mảng
+ 			var viTri = TimViTri(maNV);
+ 			// khởi tạo đối tượng nhân viên
+ 			var nhanVien = new NhanVien(maNV,tenNV,email,soDT,ngaySinh,luongCB,soNgayLamViec,chucVu,phuCap);
+ 			// Cập nhật đối tượng nhân viên trong mảng danh sách nhân viên
+ 			dsnv.SuaNhanVien(viTri,nhanVien);
+ 			
+ 			// lưu vào localstorage
+ 			LuuLocalStorage(dsnv);
+ 			HienThiLenBang(dsnv);
+ 			ResetForm();
+ 			// đóng modal lại
+ 			$('#btnClose').trigger('click');
+ 			//swal("Hoàn thành!", "Đã cập nhật thông tin nhân viên!", "success");
+ 			new PNotify({
+			    title: 'Hoàn thành!',
+			    text: 'Đã cập nhật nhân viên.',
+			    type: 'success'
+			});
 
+ 		}
+ 	});
 
+ 	// CHỨC NĂNG TÌM KIẾM
+ 	$('#btnTimKiem').click(function(){
+ 		var key = $('#key').val().trim();
+ 		key = key.toUpperCase();
+ 		var tuKhoa = `<span class='tukhoa'>${key}</span>`;
+ 		var dsnvTK = new DanhSachNhanVien(); 		
+ 		for (var i = 0; i < dsnv.DSNV.length; i++) {
+ 			
+ 			var nhanVien = dsnv.DSNV[i];
+ 			
 
+ 			if(nhanVien.MaNhanVien.toUpperCase().includes(key)&&!nhanVien.TenNhanVien.toUpperCase().includes(key)){
+ 				// lấy giá trị từ đối tượng ra
+ 				var maNV = nhanVien.MaNhanVien;
+	 			var tenNV = nhanVien.TenNhanVien;
+	 			var email = nhanVien.Email;
+	 			var soDT = nhanVien.SoDT;
+	 			var ngaySinh = nhanVien.NgaySinh;
+	 			var luongCB = nhanVien.LuongCoBan;
+	 			var soNgayLamViec = nhanVien.SoNgayLamViec;
+	 			var chucVu = nhanVien.ChucVu;
+	 			var phuCap = nhanVien.PhuCap;
 
+ 				maNV = maNV.replace(key,tuKhoa);
+ 				var nv = new NhanVien(maNV,tenNV,email,soDT,ngaySinh,luongCB,soNgayLamViec,chucVu,phuCap);
+ 				dsnvTK.ThemNhanVien(nv);
+ 			}
+ 			else if(!nhanVien.MaNhanVien.toUpperCase().includes(key)&&nhanVien.TenNhanVien.toUpperCase().includes(key)){
+ 				// lấy giá trị từ đối tượng ra
+ 				var maNV = nhanVien.MaNhanVien;
+	 			var tenNV = nhanVien.TenNhanVien;
+	 			var email = nhanVien.Email;
+	 			var soDT = nhanVien.SoDT;
+	 			var ngaySinh = nhanVien.NgaySinh;
+	 			var luongCB = nhanVien.LuongCoBan;
+	 			var soNgayLamViec = nhanVien.SoNgayLamViec;
+	 			var chucVu = nhanVien.ChucVu;
+	 			var phuCap = nhanVien.PhuCap;
+
+ 				tenNV = tenNV.replace(key,tuKhoa);
+ 				var nv = new NhanVien(maNV,tenNV,email,soDT,ngaySinh,luongCB,soNgayLamViec,chucVu,phuCap);
+ 				dsnvTK.ThemNhanVien(nv);
+ 			}
+ 			else if(nhanVien.MaNhanVien.toUpperCase().includes(key)&&nhanVien.TenNhanVien.toUpperCase().includes(key)){
+ 				// lấy giá trị từ đối tượng ra
+ 				var maNV = nhanVien.MaNhanVien;
+	 			var tenNV = nhanVien.TenNhanVien;
+	 			var email = nhanVien.Email;
+	 			var soDT = nhanVien.SoDT;
+	 			var ngaySinh = nhanVien.NgaySinh;
+	 			var luongCB = nhanVien.LuongCoBan;
+	 			var soNgayLamViec = nhanVien.SoNgayLamViec;
+	 			var chucVu = nhanVien.ChucVu;
+	 			var phuCap = nhanVien.PhuCap;
+
+	 			maNV = maNV.replace(key,tuKhoa);
+ 				tenNV = tenNV.replace(key,tuKhoa);
+ 				var nv = new NhanVien(maNV,tenNV,email,soDT,ngaySinh,luongCB,soNgayLamViec,chucVu,phuCap);
+ 				dsnvTK.ThemNhanVien(nv);
+ 			}
+ 			
+ 		}
+ 		// Hiển thị danh sách tìm kiếm được lên bảng
+ 		HienThiLenBang(dsnvTK);
+ 		NhapNhay(5,'.tukhoa');
+ 	});
+
+ 	// tạo hiệu ứng nhấp nháy
+ 	function NhapNhay(solan,id){
+ 		for(var i = 0; i < solan; i++){
+ 			$(id).fadeOut(500);
+ 			$(id).fadeIn(500);
+ 		}
+ 	}
 
 
 
