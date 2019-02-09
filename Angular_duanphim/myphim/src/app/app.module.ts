fea723792777d325dfaeeadd8ba1801a0c6ea219
nguyenminhthong2812 <45055992+nguyenminhthong2812@users.noreply.github.com>
2019-02-10 05:20:27 +0700

    16

--- Angular_duanphim/myphim/src/app/app.module.ts
+++ Angular_duanphim/myphim/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { AdminModule } from './admin/admin.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -18,7 +20,8 @@ import { AdminModule } from './admin/admin.module';
     AppRoutingModule,
     HomeModule,
     AdminModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
     
   ],
   providers: [],
