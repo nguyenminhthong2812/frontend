b18ca6c5beb354f04faa89be06e7b1ae509875c8
nguyenminhthong2812 <45055992+nguyenminhthong2812@users.noreply.github.com>
2019-02-26 05:43:01 +0700

    4

--- doan_angular/myphim/src/app/home/dangky/dangky.component.ts
+++ doan_angular/myphim/src/app/home/dangky/dangky.component.ts
@@ -11,12 +11,16 @@ import { NguoiDung } from '../models/nguoidung';
 })
 export class DangkyComponent implements OnInit {  
   public DanhSachNguoiDung:any=[];
+  public themStatus:boolean = true;
   @ViewChild('formDangKy') formDK:NgForm;
   @ViewChild('btnLuu') btnLuu:ElementRef;
   @ViewChild('taikhoan') inputTaiKhoan:ElementRef;
   constructor(private nguoidung:NguoidungService) { }
 
   ngOnInit() {
+    this.LayDanhSachNguoiDung();
+  }
+  LayDanhSachNguoiDung(){
     this.nguoidung.LayDSNguoiDung().subscribe(
       data => {
         this.DanhSachNguoiDung = data;
@@ -42,9 +46,58 @@ export class DangkyComponent implements OnInit {
       MaNhom:maNhom,
       Email:email,
       SoDT:sodt,
-      LoaiNguoiDung:loaiNguoiDung
+      MaLoaiNguoiDung:loaiNguoiDung
     });
     this.btnLuu.nativeElement.innerHTML = 'Cập nhật';
     this.inputTaiKhoan.nativeElement.readOnly = true;
+    this.themStatus = false; // tắt cờ này để cập nhật nguoidung
+  }
+  LuuNguoiDung(nguoidung){
+    if(this.themStatus){
+      // thêm người dùng
+      this.nguoidung.ThemNguoiDung(nguoidung).subscribe(
+        kq => {
+          alert('Lưu thành công!');
+          this.LayDanhSachNguoiDung();
+          this.formDK.reset();
+        },
+        loi => {
+          alert(loi);
+        }
+      );
+    }
+    else{
+      // cập nhật người dùng      
+      this.nguoidung.CapNhatNguoiDung(nguoidung).subscribe(
+        kq => {
+          alert('Cập nhật thành công!');
+          this.themStatus = true; // bật cờ lên để thêm dữ liệu
+          this.inputTaiKhoan.nativeElement.readOnly = false;
+          this.formDK.reset();
+          this.LayDanhSachNguoiDung();
+        },
+        loi => {
+          alert(loi);
+        }
+      );
+    }
+  }
+  XoaNguoiDung(taiKhoan){
+    //let taiKhoan = btnXoa.getAttribute('data-TaiKhoan');
+    if(confirm('Bạn có chắc xóa người dùng này?')){
+      this.nguoidung.XoaNguoiDung(taiKhoan).subscribe(
+        kq => {
+          alert('Xóa thành công!');
+          this.LayDanhSachNguoiDung();
+        },
+        loi => {alert(loi);}
+      );
+    }
+  }
+  ThemMoi(){
+    this.formDK.reset();
+    this.themStatus = true; // bật cờ lên để thêm dữ liệu
+    this.inputTaiKhoan.nativeElement.readOnly = false;   
+    console.log(this.themStatus); 
   }
 }
--- doan_angular/myphim/src/app/services/nguoidung.service.ts
+++ doan_angular/myphim/src/app/services/nguoidung.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,10 +8,29 @@ import { Observable } from 'rxjs';
 export class NguoidungService {
 
   constructor(private _http:HttpClient) { }
-
+  
   LayDSNguoiDung(){
     let api = `http://sv2.myclass.vn/api/QuanLyNguoiDung/LayDanhSachNguoiDung?MaNhom=GP01`;
     let Observe = this._http.get(api);
+    return Observe;
+  }
+  ThemNguoiDung(nguoidung){
+    let api = `http://sv2.myclass.vn/api/QuanLyNguoiDung/ThemNguoiDung`;
+    let header = new HttpHeaders();
+    header.append('Content-Type','application/json;charset=UTF-8');    
+    let Observe = this._http.post(api,nguoidung,{headers:header});
+    return Observe;
+  }
+  CapNhatNguoiDung(nguoidung){
+    let api = `http://sv2.myclass.vn/api/QuanLyNguoiDung/CapNhatThongTin`;
+    let header = new HttpHeaders();
+    header.append('Content-Type','application/json;charset=UTF-8');
+    let Observe = this._http.post(api,nguoidung,{headers:header});
+    return Observe;
+  }
+  XoaNguoiDung(taikhoan){
+    let api = `http://sv2.myclass.vn/api/QuanLyNguoiDung/XoaNguoiDung?TaiKhoan=${taikhoan}`;
+    let Observe = this._http.delete(api);
     return Observe;
   }
 }
